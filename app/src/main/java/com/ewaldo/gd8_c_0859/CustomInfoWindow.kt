eadd0b9be81b5ce35f0df71267fb048f93e08fac
ewaldojs <111957886+ewaldojs@users.noreply.github.com>
2022-10-27 14:37:23 +0700

    t

--- app/src/main/java/com/ewaldo/gd8_c_0859/CustomInfoWindow.kt
+++ app/src/main/java/com/ewaldo/gd8_c_0859/CustomInfoWindow.kt
@@ -5,17 +5,17 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.infowindow.InfoWindow
 
-class CustomInfoWindow(mapView: MapView?) : InfoWindow(R.layout.layout_tooltip,mapView) {
+class CustomInfoWindow(mapView: MapView?) : InfoWindow(R.layout.layout_tooltip, mapView) {
     override fun onClose() {
+
     }
 
     override fun onOpen(item: Any) {
         val marker = item as Marker
         val infoWindowData = marker.relatedObject as ModelMain
-
-        val tvNamaLokasi= mView.tvNamaLokasi
-        val tvAlamat= mView.tvAlamat
-        val imageClose= mView.imageClose
+        val tvNamaLokasi = mView.tvNamaLokasi
+        val tvAlamat = mView.tvAlamat
+        val imageClose = mView.imageClose
 
         tvNamaLokasi.text = infoWindowData.strName
         tvAlamat.text = infoWindowData.strVicinity
--- app/src/main/java/com/ewaldo/gd8_c_0859/MainActivity.kt
+++ app/src/main/java/com/ewaldo/gd8_c_0859/MainActivity.kt
@@ -18,17 +18,16 @@ import java.io.IOException
 import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
-    var modelMainList:MutableList<ModelMain> = ArrayList()
-    lateinit var mapController: MapController
-    lateinit var overlayItem: ArrayList<OverlayItem>
+    var modelMainList: MutableList<ModelMain> = ArrayList()
+    lateinit var mapController:MapController
+    lateinit var overlayItem:ArrayList<OverlayItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
-        val geoPoint = GeoPoint(-7.78165,110.414497)
+        val geoPoint = GeoPoint(-7.78165, 110.414497)
         mapView.setMultiTouchControls(true)
         mapView.controller.animateTo(geoPoint)
         mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
@@ -40,7 +39,6 @@ class MainActivity : AppCompatActivity() {
 
         getLocationMarker()
     }
-
 
     private fun getLocationMarker(){
         try{
@@ -59,18 +57,17 @@ class MainActivity : AppCompatActivity() {
                     modelMain.strName = jsonObjectResult.getString("name")
                     modelMain.strVicinity = jsonObjectResult.getString("vicinity")
 
-
                     val jsonObjectGeo = jsonObjectResult.getJSONObject("geometry")
-                    val jsonObjectLoc = jsonObjectGeo.getJSONObject("Location")
+                    val jsonObjectLoc = jsonObjectGeo.getJSONObject("location")
                     modelMain.latLoc = jsonObjectLoc.getDouble("lat")
-                    modelMain.longLoc = jsonObjectLoc.getDouble("Lng")
+                    modelMain.longLoc = jsonObjectLoc.getDouble("lng")
                     modelMainList.add(modelMain)
                 }
                 initMarker(modelMainList)
-            }catch (e: JSONException) {
+            }catch (e:JSONException) {
                 e.printStackTrace()
             }
-        } catch (ignored: IOException) {
+        }catch (ignored:IOException) {
             Toast.makeText(
                 this@MainActivity,
                 "Oops, ada yang tidak beres. Coba ulangi beberapa saat lagi.",
@@ -79,8 +76,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun  initMarker(modelList: List<ModelMain>) {
-        for (i in modelList.indices) {
+    private fun initMarker(modelList: List<ModelMain>) {
+        for(i in modelList.indices) {
             overlayItem = ArrayList()
             overlayItem.add(
                 OverlayItem(
@@ -98,11 +95,10 @@ class MainActivity : AppCompatActivity() {
             marker.position = GeoPoint(modelList[i].latLoc, modelList[i].longLoc)
             marker.relatedObject = info
             marker.infoWindow = CustomInfoWindow(mapView)
-            marker.setOnMarkerClickListener { item, arg1 ->
+            marker.setOnMarkerClickListener { item, arg1->
                 item.showInfoWindow()
                 true
             }
-
             mapView.overlays.add(marker)
             mapView.invalidate()
         }
@@ -110,16 +106,16 @@ class MainActivity : AppCompatActivity() {
 
     public override fun onResume() {
         super.onResume()
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-        if (mapView != null){
+        Configuration.getInstance().load( this, PreferenceManager.getDefaultSharedPreferences(this))
+        if(mapView != null) {
             mapView.onResume()
         }
     }
 
     public override fun onPause() {
         super.onPause()
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-        if(mapView != null){
+        Configuration.getInstance().load( this, PreferenceManager.getDefaultSharedPreferences(this))
+        if(mapView != null) {
             mapView.onPause()
         }
     }
